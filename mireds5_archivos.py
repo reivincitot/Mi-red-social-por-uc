8d3e2688b41a70c732f69026275293363ce1a38c
reivincitot <reivincitot@gmail.com>
2023-07-07 12:11:40 -0400

    Correccion de errores al guardar la informacion, se da por terminado el primer ejercicio de la semana 5

--- mireds5_archivos.py
+++ mireds5_archivos.py
@@ -2,21 +2,21 @@ import s5red as Red
 import os
 
 
-
 Red.mostrar_bienvenida()
 nombre = Red.obtener_nombre()
 print("Hola ", nombre, ", bienvenido a Mi Red")
 
-nombre_archivo = nombre+ ".user"
+nombre_archivo = nombre + ".user"
 
-#Verificamos si el archivo existe
+# Verificamos si el archivo existe
 if os.path.isfile(nombre_archivo):
     print("Leyendo datos de usuario", nombre, "desde archivo.")
-    nombre,edad,estatura_m,estatura_cm,sexo,pais, num_amigos,estado = Red.leer_datos_usuario(nombre_archivo)
-    
+    nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos, estado = Red.leer_datos_usuario(
+        nombre_archivo)
+
 
 else:
-    #En caso que el usuario no exista, consultamos por sus datos tal como lo hacÃ­amos antes
+    # En caso que el usuario no exista, consultamos por sus datos tal como lo hacÃ­amos antes
     print()
     edad = Red.obtener_edad()
     (estatura_m, estatura_cm) = Red.obtener_estatura()
@@ -25,12 +25,13 @@ else:
     num_amigos = Red.obtener_num_amigos()
     estado = ""
 
-#En ambos casos, al llegar aquÃ­ ya conocemos los datos del usuario
+# En ambos casos, al llegar aquÃ­ ya conocemos los datos del usuario
 print("Muy bien. Estos son los datos de tu perfil.")
-Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos)
+Red.mostrar_perfil(nombre, edad, estatura_m,
+                   estatura_cm, sexo, pais, num_amigos)
 Red.mostrar_mensaje(nombre, None, estado)
 
-#Ahora podemos mostrar el menu y consultar las opciones
+# Ahora podemos mostrar el menu y consultar las opciones
 opcion = 1
 while opcion != 0:
     opcion = Red.opcion_menu()
@@ -43,17 +44,21 @@ while opcion != 0:
             nombre_amigo = input("Ingresa el nombre de tu amigo o amiga: ")
             Red.mostrar_mensaje(nombre, nombre_amigo, estado)
     elif opcion == 3:
-        Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos)
+        Red.mostrar_perfil(nombre, edad, estatura_m,
+                           estatura_cm, sexo, pais, num_amigos)
     elif opcion == 4:
         edad = Red.obtener_edad()
         (estatura_m, estatura_cm) = Red.obtener_estatura()
         sexo = Red.obtener_sexo()
         pais = Red.obtener_pais()
         num_amigos = Red.obtener_num_amigos()
-        Red.mostrar_perfil(nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos)
+        Red.mostrar_perfil(nombre, edad, estatura_m,
+                           estatura_cm, sexo, pais, num_amigos)
     elif opcion == 0:
-        print("Has decidido salir. Guardando perfil en ",nombre+".user")
-        Red.guardar_datos_usuario(nombre_archivo, nombre,edad,estatura_m,estatura_cm,sexo,pais,num_amigos,estado)
-        print("Archivo",nombre+".user","guardado")
+        print("Has decidido salir. Guardando perfil en ", nombre+".user")
+        Red.guardar_datos_usuario(nombre_archivo, nombre, edad, (estatura_m, estatura_cm), sexo, pais, num_amigos, estado)
+
+
+        print("Archivo", nombre+".user", "guardado")
 
 print("Gracias por usar Mi Red. Â¡Hasta pronto!")
--- s5red.py
+++ s5red.py
@@ -10,40 +10,52 @@ def mostrar_bienvenida():
 
     """)
 
+
 def obtener_nombre():
     nombre = input("Para empezar, dime como te llamas. ").capitalize()
     return nombre
 
+
 def obtener_edad():
     agno = int(input("Para preparar tu perfil, dime en quÃ© aÃ±o naciste. "))
-    return 2017-agno-1
+    return 2023-agno-1
+
 
 def obtener_estatura():
-    estatura = float(input("CuÃ©ntame mÃ¡s de ti, para agregarlo a tu perfil. Â¿CuÃ¡nto mides? DÃ­melo en metros. "))
+    estatura = float(input(
+        "CuÃ©ntame mÃ¡s de ti, para agregarlo a tu perfil. Â¿CuÃ¡nto mides? DÃ­melo en metros. "))
     metros = int(estatura)
-    centimetros = int( (estatura - metros)*100 )
+    centimetros = int((estatura - metros)*100)
     return (metros, centimetros)
 
+
 def obtener_sexo():
-    sexo = input("Por favor, ingresa tu sexo (M=Masculino, F=Femenino): ").upper()
+    sexo = input(
+        "Por favor, ingresa tu sexo (M=Masculino, F=Femenino): ").upper()
     while sexo != 'M' and sexo != 'F':
-        sexo = input("Por favor, ingresa tu sexo (M=Masculino, F=Femenino): ").upper()
+        sexo = input(
+            "Por favor, ingresa tu sexo (M=Masculino, F=Femenino): ").upper()
     return sexo
+
 
 def obtener_pais():
     pais = input("Indica tu paÃ­s de nacimiento: ").capitalize()
     return pais
 
+
 def obtener_num_amigos():
-    amigos = int(input("Muy bien. Finalmente, cuÃ©ntame cuantos amigos tienes. "))
+    amigos = int(
+        input("Muy bien. Finalmente, cuÃ©ntame cuantos amigos tienes. "))
     return amigos
+
 
 def obtener_datos():
     n = obtener_nombre()
     e = obtener_edad()
     (em, ec) = obtener_estatura()
     na = obtener_num_amigos()
-    return (n,e,em,ec,na)
+    return (n, e, em, ec, na)
+
 
 def mostrar_perfil(nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos):
     print("--------------------------------------------------")
@@ -51,9 +63,10 @@ def mostrar_perfil(nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos
     print("Edad:     ", edad, "aÃ±os")
     print("Estatura: ", estatura_m, "m y ", estatura_cm, "centÃ­metros")
     print("Sexo:     ", sexo)
-    print("PaÃ­s:     ", pais)
+    print("País:    ", pais)
     print("Amigos:   ", num_amigos)
     print("--------------------------------------------------")
+
 
 def opcion_menu():
     print("Acciones disponibles:")
@@ -68,9 +81,12 @@ def opcion_menu():
         opcion = int(input("Ingresa una opciÃ³n: "))
     return opcion
 
+
 def obtener_mensaje():
-    mensaje = input("Ahora vamos a publicar un mensaje. Â¿QuÃ© piensas hoy? ").capitalize()
+    mensaje = input(
+        "Ahora vamos a publicar un mensaje. Â¿QuÃ© piensas hoy? ").capitalize()
     return mensaje
+
 
 def mostrar_mensaje(origen, destinatario, mensaje):
     print("--------------------------------------------------")
@@ -80,11 +96,12 @@ def mostrar_mensaje(origen, destinatario, mensaje):
         print(origen, "dice:", "@"+destinatario, mensaje)
     print("--------------------------------------------------")
 
+
 def leer_datos_usuario(nombre_archivo):
-    with open(nombre_archivo,"r") as archivo_usuario:
+    with open(nombre_archivo, "r") as archivo_usuario:
         nombre = archivo_usuario.readline().strip()
         edad = int(archivo_usuario.readline().strip())
-        estatura= float(archivo_usuario.readline().strip())
+        estatura = float(archivo_usuario.readline().strip())
         estatura_m = int(estatura)
         estatura_cm = int((estatura-estatura_m)*100)
         sexo = archivo_usuario.readline().strip()
@@ -93,11 +110,14 @@ def leer_datos_usuario(nombre_archivo):
         estado = archivo_usuario.readline().strip()
         return nombre, edad, estatura_m, estatura_cm, sexo, pais, num_amigos, estado
 
-def guardar_datos_usuario(nombre_archivo, nombre,edad,estatura,sexo,pais,estado):
-    with open(nombre_archivo,"w") as archivo_usuario:
-        archivo_usuario.write(nombre + "\n")
-        archivo_usuario.write(edad + "\n")
-        archivo_usuario.write(estatura + "\n")
-        archivo_usuario.write(sexo + "\n")
-        archivo_usuario.write(pais + "\n")
-        archivo_usuario.write(estado + "\n")
+
+def guardar_datos_usuario(archivo, nombre, edad, estatura, sexo, pais, num_amigos, estado):
+    archivo_usuario = open(archivo, "w")
+    archivo_usuario.write(nombre + "\n")
+    archivo_usuario.write(str(edad) + "\n")
+    archivo_usuario.write(str(estatura[0] + estatura[1] / 100) + "\n")
+    archivo_usuario.write(sexo + "\n")
+    archivo_usuario.write(pais + "\n")
+    archivo_usuario.write(str(num_amigos) + "\n")
+    archivo_usuario.write(estado + "\n")
+    archivo_usuario.close()
